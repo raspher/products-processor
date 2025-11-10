81f4ee9350982810d881c02c56cc479c066b125b
raspher <raspherdev@pm.me>
2025-11-10 22:56:04 +0100

    init dev env for project

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "Nix Flake for Streaming XML Reader with lxml";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in {
+    # Development shell
+    devShells.${system}.default = with pkgs; mkShell {
+      buildInputs = [
+        python313
+        python313Packages.lxml
+        libxml2
+        libxslt
+
+        nixfmt-rfc-style
+        nixd
+      ];
+
+      shellHook = ''
+        echo "Nix Flake dev shell ready with Python + lxml"
+      '';
+    };
+
+    # Docker image
+    packages.${system}.dockerImage = with pkgs; dockerTools.buildImage {
+      name = "xmlreader";
+      tag = "latest";
+      contents = [
+        python310Full
+        python310Packages.lxml
+        libxml2
+        libxslt
+        # optional: any other dependencies
+      ];
+      config = {
+        Cmd = [ "python" "/app/streaming_xml_reader.py" ];
+      };
+    };
+  };
+}
